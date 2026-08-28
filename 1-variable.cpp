05d8bde82324d03bcf1b2cf947e1beaa8548b2a4
Mukhammadrizo <mukhammadrizo.abdumutalibov@gmail.com>
2026-08-28 22:03:12 +0500

    Add files via upload

---
+++ 1-variable.cpp
@@ -0,0 +1,21 @@
+#include iostream
+int main() {
+	unsigned int age;
+	float weight;
+	double money;
+	char currency;
+
+	stdcout  How old are you  stdendl;
+	stdcin  age;
+	
+	stdcout  How many kg are you  stdendl;
+	stdcin  weight;
+
+	stdcout  How much money have you got  stdendl;
+	stdcin  money;
+
+	stdcout  Which currency ($, €, £ or etc.)n;
+	stdcin  currency;
+
+	stdcout  nAge   age  stdendl  Weight   weight  stdendl  Cash   money  currency;
+}
