af5936f542bce7095125d33ec10ef048c503bbbb
Mukhammadrizo <mukhammadrizo.abdumutalibov@gmail.com>
2026-08-29 00:16:45 +0500

    Implement switch statement for day of the week

---
+++ 3-Switch-statement.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+	unsigned short day;
+
+	std::cout << "Enter number for get day (1 - 7)" << std::endl;
+	std::cin >> day;
+
+	switch (day) {
+	case 1:
+		std::cout << "Monday";
+		break;
+
+	case 2:
+		std::cout << "Tuesday";
+		break;
+
+	case 3:
+		std::cout << "Wednesday";
+		break;
+
+	case 4:
+		std::cout << "Thursday";
+		break;
+
+	case 5:
+		std::cout << "Friday";
+		break;
+
+	case 6:
+		std::cout << "Saturday";
+		break;
+
+	case 7:
+		std::cout << "Sunday";
+		break;
+
+	default:
+		std::cout << "We could not find this day";
+		break;
+	}
+}
